037604847764db8a7b4418875da0543f3f86a94e
Drew Wyatt <drew.j.wyatt@gmail.com>
2024-12-08 20:54:09 -0500

    add day 2

---
+++ solutions/day_02/test_solution.py
@@ -0,0 +1,11 @@
+# Tests for the day solution
+from common.utils import read_input
+from .solution import part_1, part_2
+
+
+def test_part_1():
+    assert part_1(read_input("test_input.txt")) == "TODO"
+
+
+def test_part_2():
+    assert part_2(read_input("test_input.txt")) == "TODO"
