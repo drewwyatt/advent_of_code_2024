ad58ee3a2cdad809445fccf5c78875c39266e139
Drew Wyatt <drew.j.wyatt@gmail.com>
2024-12-11 20:38:41 -0500

    day 3 part 2

--- solutions/day_03/solution.py
+++ solutions/day_03/solution.py
@@ -12,8 +12,20 @@ def part_1(input: str):
     return sum
 
 
+def remove_donts(chunk: str, chunk_index: int) -> str:
+    do_index = chunk.find("do")
+    if do_index != -1:
+        return chunk[do_index + 2 :]
+    else:
+        return chunk if chunk_index == 0 else ""
+
+
 def part_2(input: str):
-    pass
+    scrubbed = ""
+    for index, chunk in enumerate(input.split("don't")):
+        scrubbed += remove_donts(chunk, index)
+
+    return part_1(scrubbed)
 
 
 def solve(input: str, part: int):
--- solutions/day_03/test_solution.py
+++ solutions/day_03/test_solution.py
@@ -15,6 +15,14 @@ def test_part_1():
     assert part_1(read_input("test_input.txt")) == 161
 
 
+"""
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+
+This corrupted memory is similar to the example from before, but this time the mul(5,5) and mul(11,8) instructions are disabled because there is a don't() instruction before them. The other mul instructions function normally, including the one at the end that gets re-enabled by a do() instruction.
+
+This time, the sum of the results is 48 (2*4 + 8*5).
+"""
+
+
 def test_part_2():
-    pass
-    # assert part_2(read_input("test_input.txt")) == "TODO"
+    assert part_2(read_input("test_input2.txt")) == 48
